483411c7e69b3f25c97b860b19b2ba46dd4bf5aa
Andreas Marek <andimarek@fastmail.fm>
2019-12-26 09:40:05 +1100

    logging

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ try {
     const dockerfilePath = core.getInput('dockerfile-path');
     console.log(`config: container port: ${containerPort}, url: ${backendUrl}, dockerfile path: ${dockerfilePath}`);
 
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+
     buildDockerImage(dockerfilePath)
         .then((imageId) => {
             return runImage(imageId, containerPort);
